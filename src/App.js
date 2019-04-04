04a5dc09d7e96116117167b2878dece75e1c11cc
MonikaZiaja <ziaja.monika@mail.com>
2019-04-04 11:54:16 +0200

    store.js changed

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 import Posts from './components/Posts'
 import PostsForm from './components/PostsForm'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware} from 'redux'
-
-const store = createStore(() => [],{}, applyMiddleware())
+import store from './store'
 
 class App extends Component {
  
